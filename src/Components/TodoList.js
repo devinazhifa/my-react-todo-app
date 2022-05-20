7602db76e1cd9e531d858132338986faf3d55d4a
devinazhifa <devinazhifa@gmail.com>
2022-05-20 21:57:56 +0700

    Step 3 done

--- src/Components/TodoList.js
+++ src/Components/TodoList.js
@@ -2,16 +2,13 @@ import React from "react";
 import TodoItem from './TodoItem.js'
 import Header from './Header.js'
 import Footer from "./Footer.js";
+import PaginateList from "./PaginateList.js";
 
 const TodoList = (props) => {
   return (
     <div className="todo-list">
       <Header title={props.title} addNewItem={props.addNewItem} />
-      <ul className="list-group list-group-flush">
-        {props.items.map((item, i) => (
-          <TodoItem item={item} key={i} />
-        ))}
-      </ul>
+      <PaginateList items={props.items} initialPage={1} itemPerPage={2} />
       <Footer items={props.items} />
     </div>
   );
